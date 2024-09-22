bfc36c6e2ae046373520f54719abf0059e7f7603
Laila Alboloushi <laila.sb@outlook.com>
2024-09-22 14:02:38 +0200

    DB added

--- sql/db.sql
+++ sql/db.sql
@@ -1,10 +1,27 @@
+
+
 DROP DATABASE IF EXISTS subscriptions;
 CREATE DATABASE subscriptions;
 USE subscriptions;
 
 CREATE TABLE subscription_info (
-                                   id INTEGER NOT NULL PRIMARY KEY,
-                                   subName VARCHAR (50) NOT NULL,
-                                   subType VARCHAR (50),
+                                   sub_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+                                   sub_name VARCHAR (100) NOT NULL,
+                                   sub_type VARCHAR (100),
                                    membership VARCHAR (100)
 );
+
+
+INSERT INTO subscription_info (sub_name , sub_type , membership)
+VALUES
+
+    ('Netflix','Entertainment', 'active'),
+    ('Amazon Prime', 'Entertainment','not active'),
+    ('Fitness First', 'Health', 'active'),
+    ('Odd Box', 'Groceries', 'not active'),
+    ('Spotify', 'Music', 'active'),
+    ('Code Academy', 'Education', 'not active'),
+    ('Apple TV+', 'Entertainment', 'active'),
+    ('Costco Membership', 'Groceries', 'not active'),
+    ('Nike Run Club', 'Health', 'active'),
+    ('Good Reads','Entertainment', 'not active');
