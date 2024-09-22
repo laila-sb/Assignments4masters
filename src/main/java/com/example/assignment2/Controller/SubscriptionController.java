1e4fcde1fd178cec73d1635d2af3b186215074e2
Laila Alboloushi <laila.sb@outlook.com>
2024-09-22 16:33:27 +0200

    Delete request now working, del chosen subscription by sub_id

--- src/main/java/com/example/assignment2/Controller/SubscriptionController.java
+++ src/main/java/com/example/assignment2/Controller/SubscriptionController.java
@@ -3,6 +3,8 @@ package com.example.assignment2.Controller;
 import com.example.assignment2.model.Subscription;
 import com.example.assignment2.repository.SubscriptionRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,7 +13,7 @@ import java.util.List;
 @RestController
 @Slf4j
 public class SubscriptionController {
-
+    private static final Logger logger = LoggerFactory.getLogger(SubscriptionController.class);
 
     @Autowired  // in order to use the repo in the controller class
     private SubscriptionRepository subscriptionRepository;
@@ -26,7 +28,13 @@ public class SubscriptionController {
 //        return ResponseEntity.ok(new Subscription());
 //
 //    }
-    @DeleteMapping
-private void deleteSubscription(@PathVariable int sub_id){
+    @DeleteMapping("/subs/{sub_id}")
+private void deleteSubscription(@PathVariable Long sub_id){
+        if (subscriptionRepository.existsById(sub_id)) {
+            subscriptionRepository.deleteById(sub_id);
+            logger.info("Deleted Subscription with ID: {}", sub_id);
+        }else{
+            logger.error("Subscription with ID: {} not found", sub_id);
+        }
     }
 }
